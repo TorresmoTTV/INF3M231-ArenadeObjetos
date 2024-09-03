f8fdbeabe37826496c30137d96a69236fb35f0a3
TorresmoTTV <grand_turismo8@hotmail.com>
2024-09-03 10:19:00 -0300

    Primeiro Envio

--- src/model/Pessoa.java
+++ src/model/Pessoa.java
@@ -1,5 +1,49 @@
 package model;
 
 public class Pessoa {
-
+    private String nome;
+    private String cabelo;
+    private String olho;
+    private String pele;
+    private boolean sexo;
+    private int pontosDeVida;
+    
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getCabelo() {
+        return cabelo;
+    }
+    public void setCabelo(String cabelo) {
+        this.cabelo = cabelo;
+    }
+    public String getOlho() {
+        return olho;
+    }
+    public void setOlho(String olho) {
+        this.olho = olho;
+    }
+    public String getPele() {
+        return pele;
+    }
+    public void setPele(String pele) {
+        this.pele = pele;
+    }
+    public boolean isSexo() {
+        return sexo;
+    }
+    public void setSexo(boolean sexo) {
+        this.sexo = sexo;
+    }
+    public int getPontosDeVida() {
+        return pontosDeVida;
+    }
+    public void setPontosDeVida(int pontosDeVida) {
+        this.pontosDeVida = pontosDeVida;
+    }
+    
+    
 }
