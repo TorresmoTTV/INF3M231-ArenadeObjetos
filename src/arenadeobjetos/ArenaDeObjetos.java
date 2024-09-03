f776cf6f3dfcea0bc33fbf7667c93d79401306be
TorresmoTTV <grand_turismo8@hotmail.com>
2024-09-03 10:33:29 -0300

    Terceiro Envio

---
+++ src/arenadeobjetos/ArenaDeObjetos.java
@@ -0,0 +1,11 @@
+package arenadeobjetos;
+
+import model.Pessoa;
+
+public class ArenaDeObjetos {
+    public static void main(String[] args) {
+
+        Pessoa vitima = new Pessoa("Vitima 1", "Castanho", "Castanho Claro", "Branca", true, 100);
+
+    }
+}
