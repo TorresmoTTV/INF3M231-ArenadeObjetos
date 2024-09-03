fc4ccb13c9f9bf143993cf4a18df6bfcf889a365
TorresmoTTV <grand_turismo8@hotmail.com>
2024-09-03 10:22:19 -0300

    Segundo Envio

--- src/model/Pessoa.java
+++ src/model/Pessoa.java
@@ -7,48 +7,71 @@ public class Pessoa {
     private String pele;
     private boolean sexo;
     private int pontosDeVida;
-    
+
+    public Pessoa() {
+    }
+
+    public Pessoa(String nome, String cabelo, String olho, String pele, boolean sexo, int pontosDeVida) {
+        this.nome = nome;
+        this.cabelo = cabelo;
+        this.olho = olho;
+        this.pele = pele;
+        this.sexo = sexo;
+        this.pontosDeVida = pontosDeVida;
+    }
+
     public String getNome() {
         return nome;
     }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
+
     public String getCabelo() {
         return cabelo;
     }
+
     public void setCabelo(String cabelo) {
         this.cabelo = cabelo;
     }
+
     public String getOlho() {
         return olho;
     }
+
     public void setOlho(String olho) {
         this.olho = olho;
     }
+
     public String getPele() {
         return pele;
     }
+
     public void setPele(String pele) {
         this.pele = pele;
     }
+
     public boolean isSexo() {
         return sexo;
     }
+
     public void setSexo(boolean sexo) {
         this.sexo = sexo;
     }
+
     public int getPontosDeVida() {
         return pontosDeVida;
     }
+
     public void setPontosDeVida(int pontosDeVida) {
         this.pontosDeVida = pontosDeVida;
     }
+
     @Override
     public String toString() {
         return "Pessoa [nome=" + nome + ", cabelo=" + cabelo + ", olho=" + olho + ", pele=" + pele + ", sexo=" + sexo
                 + ", pontosDeVida=" + pontosDeVida + "]";
     }
-    
-    
+
 }
